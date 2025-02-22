7170dcab11b43e89acc623ef5ae99676999d03c1
Tom K <tomknighton@icloud.com>
2025-02-22 16:08:49 +0000

    build: log levels

--- GoTravel.Connector.Services/ServiceCollections/SerilogCollection.cs
+++ GoTravel.Connector.Services/ServiceCollections/SerilogCollection.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
+using Serilog.Events;
 
 namespace GoTravel.Connector.Services.ServiceCollections;
 
@@ -9,6 +10,8 @@ public static class SerilogCollection
     {
         Log.Logger = new LoggerConfiguration()
             .WriteTo.Console()
+            .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
             .CreateLogger();
 
         return services;
